91f3a9e9ba2a2b6286925e02f6eb85df9e49b273
GuilleAmutio <56632305+GuilleAmutio@users.noreply.github.com>
2021-01-26 13:11:28 +0100

    Delete Jenkins directory

--- Jenkins/scanner_conf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-COOKIEJAR="$(mktemp)"
-CRUMB=$(curl -u "admin:admin" --cookie-jar "$COOKIEJAR" "http://10.0.0.3:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)")
-wget https://github.com/GuilleAmutio/DevOpsChallenge/raw/main/Jenkins/conf_jenkins_sonar.groovy
-curl -d "script=$(< ./conf_jenkins_sonar.groovy)" -v --cookie "$COOKIEJAR" -H "$CRUMB" -u "admin:admin" "http://10.0.0.3:8080/script"
