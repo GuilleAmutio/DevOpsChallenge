7e8d1fa8925b8e203570127894b7ebd0b8b24ddc
GuilleAmutio <56632305+GuilleAmutio@users.noreply.github.com>
2021-01-28 08:27:52 +0100

    Update token_credentials.sh

--- Jenkins/token_credentials.sh
+++ Jenkins/token_credentials.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-token=$(curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "name=WindowsTokenFinal" -u admin:admin 10.0.0.2:9000/api/user_tokens/generate | jq -r '.token')
+token=$(curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "name=WindowsTokenFinal" -u admin:admin 10.0.0.2:9000/api/user_tokens/generate | jq -r '.token' | xargs)
 COOKIEJAR="$(mktemp)"
 CRUMB=$(curl -u "admin:admin" --cookie-jar "$COOKIEJAR" "http://10.0.0.3:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)")
-curl -X POST -u "admin:admin" --cookie "$COOKIEJAR" -H "$CRUMB" 'http://10.0.0.3:8080/credentials/store/system/domain/_/createCredentials' --data-urlencode 'json={"": "0","credentials": {"scope": "GLOBAL","id": "WindowsAutoToken","description": "Automatically generated sonar token from windows","secret": "$Token", "$class": "org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl"}}'
+curl -X POST -u "admin:admin" --cookie "$COOKIEJAR" -H "$CRUMB" 'http://10.0.0.3:8080/credentials/store/system/domain/_/createCredentials' --data-urlencode 'json={"": "0","credentials": {"scope": "GLOBAL","id": "WindowsAutoToken","description": "Automatically generated sonar token from windows","secret": "$token", "$class": "org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl"}}'
