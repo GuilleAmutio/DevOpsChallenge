68da9c0d1e339476610a1995d64f8a5e59664cbf
GuilleAmutio <56632305+GuilleAmutio@users.noreply.github.com>
2021-02-13 17:27:16 +0100

    Delete Jenkins directory

--- Jenkins/jenkins_conf_installations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-COOKIEJAR="$(mktemp)"
-CRUMB=$(curl -u "admin:admin" --cookie-jar "$COOKIEJAR" "http://10.0.0.3:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)")
-wget https://github.com/GuilleAmutio/DevOpsChallenge/raw/main/Jenkins/conf_jenkins_sonar.groovy
-curl -d "script=$(< ./conf_jenkins_sonar.groovy)" -v --cookie "$COOKIEJAR" -H "$CRUMB" -u "admin:admin" "http://10.0.0.3:8080/script"
-wget https://github.com/GuilleAmutio/DevOpsChallenge/raw/main/Jenkins/conf_maven.groovy
-curl -d "script=$(< ./conf_maven.groovy)" -v --cookie "$COOKIEJAR" -H "$CRUMB" -u "admin:admin" "http://10.0.0.3:8080/script"
--- Jenkins/webhook_sonar.sh
+++
@@ -1,1 +0,0 @@
-curl "http://admin:admin@10.0.0.2:9000/api/webhooks/create" -X POST -d "name=jenkins&url=http://10.0.0.3:8080/sonarqube-webhook/"
